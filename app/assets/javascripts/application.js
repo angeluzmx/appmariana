ea0a9b79395bef7aeb78b91571b22ff1ffb1213f
angel <manvnd@gmail.com>
2016-04-05 13:06:49 -0500

    se modifico application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require turbolinks
 //= require_tree .
- //= require bootstrap
+
 
